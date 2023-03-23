08dc15c1c038bad36435c94056966d164b97e0bd
MicheleTosi <micheletosi98@gmail.com>
2023-03-23 12:47:39 +0100

    added my name

--- Prova_JIRA/src/Main.java
+++ Prova_JIRA/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Marina!");
         System.out.println("Hello Arianna!");
+        System.out.println("I'm Michele");
     }
 }
